6a6e557a5361642f6b7dbc1e0f651c098673e05a
alaa chaar <alaachaar774@gmail.com>
2024-10-10 16:17:38 +0100

    login done and auth middleware ready to use in jobs queries

--- Jobs-Api/starter/app.js
+++ Jobs-Api/starter/app.js
@@ -7,7 +7,9 @@ const app = express()
 
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
+
 const connectDB = require('./db/connect')
+const authenticateUser= require('./middleware/auth')
 //error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -19,7 +21,7 @@ app.use(express.json())
 
 //routes
 app.use('/api/v1/auth',authRouter)
-app.use('/api/v1/jobs',jobsRouter)
+app.use('/api/v1/jobs',authenticateUser,jobsRouter)
 
 
 app.use(notFoundMiddleware)
--- Jobs-Api/starter/controllers/auth.js
+++ Jobs-Api/starter/controllers/auth.js
@@ -1,7 +1,8 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require('../errors')
-const bcrypt = require('bcryptjs')
+const {BadRequestError, UnauthenticatedError} = require('../errors')
+
+
 
 const register = async(req,res)=>{
     // handle errors through controller
@@ -11,11 +12,32 @@ const register = async(req,res)=>{
     // }
     
     const user = await User.create({...req.body})
-    res.status(StatusCodes.CREATED).json({ user })
+    const token = user.createJwt()
+    res.status(StatusCodes.CREATED).json({user:{name: user.name} ,token }) // i need the name for the frontend 
 }
 
 const login = async(req,res)=>{
-    res.send('login user')
+    const {email , password} = req.body
+
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({email})
+    
+    if(!user){
+        throw new UnauthenticatedError('email does not exist')
+    }
+
+    //email exists now we will compare password
+    const isPasswordCorrect = await user.comparePassword(password)
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('wrong password')
+    }
+    
+
+    const token = user.createJwt()
+    res.status(StatusCodes.OK).json( {user :{name:user.name} , token} )
 }
 
 module.exports = {
